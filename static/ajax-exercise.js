e303a53c460cc72d3b8674a18131af7dd40059ed
Hackbright Student <no-reply@hackbrightacademy.com>
2017-10-27 17:00:32 -0700

    mostly done with order melons

--- static/ajax-exercise.js
+++ static/ajax-exercise.js
@@ -36,13 +36,27 @@ $("#weather-form").on('submit', showWeather);
 
 // PART 3: ORDER MELONS
 
+function melonDisplay(results) {
+   console.log(results);
+//still need to format for error
+
+   $('#order-status').append(results.code);
+   $('#order-status').html(results.msg);
+}
+
 function orderMelons(evt) {
     evt.preventDefault();
 
+    let url = '/order-melons.json';
+    let formData = {'qty': $('#qty-field').val(), 
+                    'melon_type': $('#melon-type-field').val()};
+
+    $.post(url, formData, melonDisplay);
+    console.log('sent data');
     // TODO: show the result message after your form
     // TODO: if the result code is ERROR, make it show up in red (see our CSS!)
 }
-
+//either order-form-button on click or order-form on submit. either is fine.
 $("#order-form").on('submit', orderMelons);
 
 
